9755cc6b92590217a2c11509644a4442212052a2
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-10-26 15:27:29 -0300

    Event Listener of Add To Cart Button started

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,33 @@
+const cartButton = document.querySelector('[data-add-to-cart]')
+
 document.querySelectorAll('.quantity-input').forEach(function (element) {
-    const input = element.querySelector('input');
-    const minus = element.querySelector('.minus');
-    const plus = element.querySelector('.plus');
+    const input = element.querySelector('input')
+    const minus = element.querySelector('.minus')
+    const plus = element.querySelector('.plus')
   
-    minus.addEventListener('click', function () {
-        let value = parseFloat(input.value) || 0;
+    minus.addEventListener('click', () => {
+        let value = parseFloat(input.value) || 0
         if (value > 1) {
-            input.value = value - 1;
+            input.value = value - 1
         }
-    });
+    })
   
-    plus.addEventListener('click', function () {
-        let value = parseFloat(input.value) || 0;
-        input.value = value + 1;
-    });
-});
+    plus.addEventListener('click', () => {
+        let value = parseFloat(input.value) || 0
+        input.value = value + 1
+    })
+})
+
+cartButton.addEventListener('click', () => {
+    let productName = document.querySelector('.product-info h1[data-product-name]').textContent
+    let productCurrentPrice = document.querySelector('.product-info [data-price="current-price"]').innerHTML
+    let productDiscount = document.querySelector('.product-info [data-price="discount"]').innerHTML
+    let productOldPrice = document.querySelector('.product-info [data-price="old-price"]').innerHTML
+    let productQuantity = document.querySelector('.product-info [data-product-quantity]').value
+
+    console.log(productName)
+    console.log(productCurrentPrice)
+    console.log(productDiscount)
+    console.log(productOldPrice)
+    console.log(productQuantity)
+})
