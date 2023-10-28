dc29aa815b40ee5a7a7dc4e80ac7603f091e50b3
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-10-28 13:11:43 -0300

    Declare variable to get the main image src

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,14 +20,16 @@ document.querySelectorAll('.quantity-input').forEach(function (element) {
 
 cartButton.addEventListener('click', () => {
     let productName = document.querySelector('.product-info h1[data-product-name]').textContent
+    let productMainImage = document.querySelector('figure.main-image [data-main-image] img').getAttribute('src')
     let productCurrentPrice = document.querySelector('.product-info [data-price="current-price"]').innerHTML
     let productDiscount = document.querySelector('.product-info [data-price="discount"]').innerHTML
     let productOldPrice = document.querySelector('.product-info [data-price="old-price"]').innerHTML
     let productQuantity = document.querySelector('.product-info [data-product-quantity]').value
     
     const cartContainer = document.querySelector('header nav [data-cart]')
-    
+
     console.log(productName)
+    console.log(productMainImage)
     console.log(productCurrentPrice)
     console.log(productDiscount)
     console.log(productOldPrice)
