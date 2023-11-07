dfff9034d07e06ee5652a633931338f0f6006447
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-11-07 10:21:13 -0300

    Cleaning code

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,11 +22,8 @@ cartButton.addEventListener('click', () => {
     let productName = document.querySelector('.product-info h1[data-product-name]').textContent
     let productMainImage = document.querySelector('figure.main-image [data-main-image] img').getAttribute('src')
     let productCurrentPrice = document.querySelector('.product-info [data-price="current-price"]').innerHTML
-    let productDiscount = document.querySelector('.product-info [data-price="discount"]').innerHTML
-    let productOldPrice = document.querySelector('.product-info [data-price="old-price"]').innerHTML
     let productQuantity = document.querySelector('.product-info [data-product-quantity]').value
     
-    const cartContainer = document.querySelector('header nav [data-cart]')
     const emptyCartContainer = document.querySelector('header nav [data-cart] [data-empty-cart]')
     const filledCartContainer = document.querySelector('header nav [data-cart] [data-filled-cart]')
     const cartList = document.querySelector('header nav [data-cart] [data-filled-cart] ul.products-in-cart')
@@ -51,7 +48,7 @@ cartButton.addEventListener('click', () => {
     itemTitle.className = 'item-title'
 
     let totalPrice = productCurrentPrice * productQuantity
-    const itemText = document.createTextNode(productName + '<br>' + productCurrentPrice + ' ' + productQuantity + ' ' + totalPrice)
+    const itemText = document.createTextNode(productName + '<br>' + productCurrentPrice + ' x ' + productQuantity + ' ' + totalPrice)
     itemTitle.appendChild(itemText)
 
     const deleteButton = document.createElement('button')
