a14279278fb6df2cdbeb74b7bdc74769240efac7
Melksedeque Silva <freelancer@melksedeque.com.br>
2023-11-11 17:28:03 -0300

    Cleaning JS code

--- assets/js/image-navigation.js
+++ assets/js/image-navigation.js
@@ -1,44 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const mainImage = document.querySelector(".main-image img");
-  const thumbnails = document.querySelectorAll(".thumbs-navigation img");
-  const lightbox = document.createElement("div");
-  lightbox.classList.add("lightbox");
-  document.body.appendChild(lightbox);
+  const mainImage = document.querySelector(".main-image img")
+  const thumbnails = document.querySelectorAll(".thumbs-navigation img")
+  const lightbox = document.createElement("div")
+  lightbox.classList.add("lightbox")
+  document.body.appendChild(lightbox)
 
   // Função para mudar a imagem principal
   function changeMainImage(imgSrc) {
-    mainImage.src = imgSrc;
+    mainImage.src = imgSrc
   }
 
   // Evento de clique nas miniaturas
   thumbnails.forEach((thumb) => {
     thumb.addEventListener("click", function () {
-      changeMainImage(this.dataset.target);
-    });
-  });
+      changeMainImage(this.dataset.target)
+    })
+  })
 
   // Evento de clique na imagem principal para abrir o lightbox
   mainImage.addEventListener("click", function () {
-    const lightboxImage = document.createElement("img");
-    lightboxImage.src = this.src;
+    const lightboxImage = document.createElement("img")
+    lightboxImage.src = this.src
     while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild);
+      lightbox.removeChild(lightbox.firstChild)
     }
-    lightbox.appendChild(lightboxImage);
-    lightbox.style.display = "flex";
-  });
+    lightbox.appendChild(lightboxImage)
+    lightbox.style.display = "flex"
+  })
 
   // Evento de clique no lightbox para fechar
   lightbox.addEventListener("click", function () {
-    lightbox.style.display = "none";
-  });
+    lightbox.style.display = "none"
+  })
 
   // Carrossel automático para mobile
   if (window.innerWidth <= 600) {
-    let currentThumbIndex = 0;
+    let currentThumbIndex = 0
     setInterval(() => {
-      currentThumbIndex = (currentThumbIndex + 1) % thumbnails.length;
-      changeMainImage(thumbnails[currentThumbIndex].dataset.target);
-    }, 3000); 
+      currentThumbIndex = (currentThumbIndex + 1) % thumbnails.length
+      changeMainImage(thumbnails[currentThumbIndex].dataset.target)
+    }, 3000) 
   }
-});
+})
